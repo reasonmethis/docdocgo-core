abd078995d2969ae87b2e19f0b6d1b5ef5069040
Dmitriy Vasilyuk <111213624+reasonmethis@users.noreply.github.com>
2023-11-18 09:44:00 -0800

    Improve doc ingestion output + update default db

--- ingest_local_docs.py
+++ ingest_local_docs.py
@@ -5,10 +5,24 @@ import os
 import sys
 from langchain.document_loaders import TextLoader, DirectoryLoader
 from utils import docgrab
+from utils.helpers import is_directory_empty
 
 if __name__ == "__main__":
     DOCS_TO_INGEST_DIR_OR_FILE = os.getenv("DOCS_TO_INGEST_DIR_OR_FILE")
     SAVE_VECTORDB_DIR = os.getenv("SAVE_VECTORDB_DIR")
+
+    if not DOCS_TO_INGEST_DIR_OR_FILE or not SAVE_VECTORDB_DIR:
+        print("Please set  DOCS_TO_INGEST_DIR_OR_FILE and SAVE_VECTORDB_DIR in `.env`.")
+        sys.exit()
+    if not os.path.exists(DOCS_TO_INGEST_DIR_OR_FILE):
+        print(f"{DOCS_TO_INGEST_DIR_OR_FILE} does not exist.")
+        sys.exit()
+    if not os.path.isdir(SAVE_VECTORDB_DIR):
+        print(f"{SAVE_VECTORDB_DIR} is not an existing directory.")
+        sys.exit()
+    if not is_directory_empty(SAVE_VECTORDB_DIR):
+        print(f"{SAVE_VECTORDB_DIR} is not empty. Please specify an empty directory.")
+        sys.exit()
 
     print(f"You are about to ingest documents from: {DOCS_TO_INGEST_DIR_OR_FILE}")
     print(f"The vector database will be saved to: {SAVE_VECTORDB_DIR}\n")
